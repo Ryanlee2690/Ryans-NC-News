62ee99afff49ca5512ba93253423756d1545f7aa
Ryan Nuttall <ryannuttall1996@hotmail.com>
2022-03-29 12:01:26 +0100

    Removed error handling that wasnt needed

--- models/news.model.js
+++ models/news.model.js
@@ -13,9 +13,6 @@ exports.selectAllTopics = () => {
 exports.selectAllUsernames = () => {
     return db.query('SELECT username FROM users')
     .then(({rows}) => {
-        if (rows.length === 0) {
-            return Promise.reject({ msg: "not found", status: 404 })
-        }
         return rows
     });
 };
